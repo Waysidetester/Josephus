399212a0206f70e607bacf12066145d603897341
WaysideTester <dylanmurray211@outlook.com>
2019-03-09 11:13:59 -0600

    removing every 3rd item from collection

--- Josephus/Program.cs
+++ Josephus/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Josephus
 {
@@ -6,7 +7,23 @@ namespace Josephus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<short> NumOfSoliders = new List<short>
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            short ThirdManVerifier = 1;
+            while (NumOfSoliders.Count > 1)
+            {
+                for (int i = 0; i < NumOfSoliders.Count; i++)
+                {
+                    if (ThirdManVerifier == 3)
+                    {
+                        ThirdManVerifier = 1;
+                        NumOfSoliders.RemoveAt(i);
+                    }
+                    ThirdManVerifier++;
+                }
+            }
+            Console.WriteLine(NumOfSoliders[0].ToString());
+            Console.ReadLine();
         }
     }
 }
