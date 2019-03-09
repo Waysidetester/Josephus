f105ac1922176c5d74609752664fcb1503c9ae44
WaysideTester <dylanmurray211@outlook.com>
2019-03-09 15:07:40 -0600

    Tracking index and removing at end of loop

--- Josephus/Program.cs
+++ Josephus/Program.cs
@@ -8,21 +8,33 @@ namespace Josephus
         static void Main(string[] args)
         {
             List<short> NumOfSoliders = new List<short>
-            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
             short ThirdManVerifier = 1;
             while (NumOfSoliders.Count > 1)
             {
+                List<int> RemovedIndexPosition = new List<int>();
                 for (int i = 0; i < NumOfSoliders.Count; i++)
                 {
                     if (ThirdManVerifier == 3)
                     {
                         ThirdManVerifier = 1;
-                        NumOfSoliders.RemoveAt(i);
+                        Console.WriteLine(NumOfSoliders[i]);
+                        RemovedIndexPosition.Add(i);
+
                     }
-                    ThirdManVerifier++;
+                    else
+                    {
+                        ThirdManVerifier++;
+                    }
+                }
+                RemovedIndexPosition.Reverse();
+                foreach (var index in RemovedIndexPosition)
+                {
+                    NumOfSoliders.RemoveAt(index);
                 }
             }
-            Console.WriteLine(NumOfSoliders[0].ToString());
+            Console.WriteLine("End: " + NumOfSoliders[0].ToString());
             Console.ReadLine();
         }
     }
